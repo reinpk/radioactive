9b783843d11a9f61cc0170ebc6e1e45f09b62312
Peter Reinhardt <peter@segment.io>
2013-01-12 22:27:41 -0800

    getting first data and fixing some tstyles

--- scripts/decay-demo.js
+++ scripts/decay-demo.js
@@ -2,11 +2,58 @@
 // Data
 // ----
 
+var E = function (exponent) {
+    return Math.pow(10, exponent);
+};
+
 var reactors = [
     {
-        name : 'Light Water',
-        slug : 'light-water',
-        wasteProfile : [],
+        name : 'Pressurized Water',
+        slug : 'pressurized-water',
+        wasteProfile : {
+            // units are kg/MTU initial
+            'Am-241' : 1.400*E(-1),
+            'Cm-242' : 1.190*E(-2),
+            'Cm-244' : 1.650*E(-2),
+            'Cs-134' : 9.540*E(-2),
+            'Cs-137' : 1.000*E(0),
+            'Eu-154' : 1.970*E(-2),
+            'Pu-238' : 1.060*E(-1),
+            'Pu-239' : 5.080*E(0),
+            'Pu-240' : 2.040*E(0),
+            'Pu-241' : 1.110*E(0),
+            'Pu-242' : 3.660*E(-1),
+            'U-235'  : 1.010*E(1),
+            'U-236'  : 4.050*E(0),
+            'U-238'  : 9.480*E(2)
+        },
+        wasteDataSource : 'http://www.oecd-nea.org/sfcompo/Ver.2/search/search.pl?rosin=Obrigheim&cell=BE124&pin=G7&axis=2315',
+        wasteAnalysis : '',
+        description   : '',
+        diagram       : ''
+    },
+    {
+        name : 'Boiling Water',
+        slug : 'boiling-water',
+        wasteProfile : {
+            // units are kg/MTU initial
+            'Am-241' : 6.630*E(-1),
+            'Cm-242' : 1.460*E(-2),
+            'Cm-244' : 1.980*E(-2),
+            'Cs-134' : 6.580*E(-2),
+            'Cs-137' : 8.630*E(-1),
+            'Eu-154' : 1.770*E(-2),
+            'Pu-236' : 1.550*E(-6),
+            'Pu-238' : 1.080*E(-1),
+            'Pu-239' : 4.800*E(0),
+            'Pu-240' : 2.170*E(0),
+            'Pu-241' : 1.140*E(0),
+            'Pu-242' : 4.500*E(-1),
+            'U-235'  : 6.500*E(0),
+            'U-236'  : 3.260*E(0),
+            'U-238'  : 9.520*E(2)
+        },
+        wasteDataSource : 'http://www.oecd-nea.org/sfcompo/Ver.2/search/search.pl?rosin=Gundremmingen&cell=B23&pin=A1&axis=2680',
         wasteAnalysis : '',
         description   : '',
         diagram       : ''
@@ -140,7 +187,7 @@ DecayDemo.ResultsView = Backbone.View.extend({
     },
 
     render : function () {
-        this.$('.demo-results-title').text(this.model.get('name') + ' Reactor Waste Profile');
+        this.$('.demo-results-title').text(this.model.get('name') + ' Reactor Waste');
         this.$('.demo-waste-analysis').html(this.model.get('wasteAnalysis'));
         this.$('.demo-reactor-description').html(this.model.get('description'));
 
@@ -150,6 +197,7 @@ DecayDemo.ResultsView = Backbone.View.extend({
 
     renderGraph : function () {
         var wasteProfile = this.model.get('wasteProfile');
+        
         // TODO run through nuclear.js
         // TODO graph it
     }
