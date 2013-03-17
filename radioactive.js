5b50c29e6813cbaf2e17ddf031d4ad340e041fc2
Peter Reinhardt <peter@segment.io>
2013-03-17 15:31:13 -0700

    rebuilding

--- radioactive.js
+++ radioactive.js
@@ -491,20 +491,22 @@ require.register("reinpk-zeroes/index.js", function(exports, require, module){
  * Create a zeroes array with the given `dimensions`.
  *
  * @param {Number|Array} dimensions
+ * @param {Number} initialValue
  * @return {Array}
  * @api public
  */
 
-function zeroes (dimensions) {
+function zeroes (dimensions, initialValue) {
   var array;
 
   if (dimensions.length === 1) dimensions = dimensions[0];
+  if (initialValue === undefined) initialValue = 0;
 
   // if it's a number, create a flat array of zeroes
   if (typeof dimensions === 'number') {
     array = new Array(dimensions);
     for (var i = 0; i < dimensions; i += 1) {
-      array[i] = 0;
+      array[i] = initialValue;
     }
   }
   // else create an array of one-dimension-less arrays full of zeroes
