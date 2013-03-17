4cfc2658e48a3fd6853d2488cd43744eb083e529
Peter Reinhardt <peter@segment.io>
2013-03-17 14:03:30 -0700

    component-ifying the split up files

--- src/convert.js
+++ src/convert.js
@@ -1,25 +1,26 @@
-// date conversion utilities
-    var convert = {
-        E : function (exponent) {
-            return Math.pow(10, exponent);
-        },
-        years : function (years) {
-            return years;
-        },
-        days : function (days) {
-            return ( days / 365.25);
-        },
-        hours : function (hours) {
-            return ( hours / (24 * 365.25) );
-        },
-        minutes : function (minutes) {
-            return ( minutes / (60 * 24 * 365.25) );
-        },
-        seconds : function (seconds) {
-            return ( seconds / (60 * 60 * 24 * 365.25) );
-        },
+// date and unit conversion utilities
 
-        moles : function (moles) {
-            return (moles * 6.02214179 * Math.pow(10, 23));
-        }
-    };
+module.exports = {
+    E : function (exponent) {
+        return Math.pow(10, exponent);
+    },
+    years : function (years) {
+        return years;
+    },
+    days : function (days) {
+        return ( days / 365.25);
+    },
+    hours : function (hours) {
+        return ( hours / (24 * 365.25) );
+    },
+    minutes : function (minutes) {
+        return ( minutes / (60 * 24 * 365.25) );
+    },
+    seconds : function (seconds) {
+        return ( seconds / (60 * 60 * 24 * 365.25) );
+    },
+
+    moles : function (moles) {
+        return (moles * 6.02214179 * Math.pow(10, 23));
+    }
+};
